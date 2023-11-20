ac37197e68c9f5e9b0eb32fece1c3f2a1aed0644
Descartes <decartes.saianval@gmail.com>
2023-11-20 12:35:24 +0100

    ✅ first commit project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './components/pages/login/LoginPage';
+import ErrorPage from './components/pages/error/ErrorPage';
+import MyStrenghtList from './components/pages/MyStrenghtList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* <Route exact path="/" element={<LoginPage />} /> */}
+        <Route exact path="/" element={<MyStrenghtList />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
